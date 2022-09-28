31105dc6561ca88150ece5e02815226e5123ba1a
LAPTOP-D85G2K3L\Plantura <pradeep.pandey@plantura.garden>
2022-09-28 14:12:27 +0545

    feat: modfiy landing page

--- App.tsx
+++ App.tsx
@@ -78,20 +78,13 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <View
+            style={{
+              backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            }}
+          />
+          <Text testID="hello">Hello, Detox!</Text>
+          <Section title="Step One"></Section>
         </View>
       </ScrollView>
     </SafeAreaView>
